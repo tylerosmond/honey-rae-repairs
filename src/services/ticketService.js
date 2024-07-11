9c4f2294d6bf9b54a8039e30c26bc13fcdb718ad
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-07-11 16:10:46 -0500

    Refactored code and added assignee functions to tickets

--- src/services/ticketService.js
+++
@@ -1,3 +0,0 @@
-export const getAllTickets = () => {
-    return fetch(`http://localhost:8088/serviceTickets`).then((res) => res.json())
-}
---
+++ src/services/ticketService.jsx
@@ -0,0 +1,5 @@
+export const getAllTickets = () => {
+  return fetch(
+    `http://localhost:8088/serviceTickets?_embed=employeeTickets`
+  ).then((res) => res.json());
+};
